7cfbeff8dc6bf13bf305fe57c9916665782661b2
Rahul Aswani <aswani@olacabs.com>
2015-05-01 19:49:19 +0530

    add facebook sdk

---
+++ app/src/main/java/com/rahulaswani/indianstartups/DataManager.java
@@ -0,0 +1,64 @@
+package com.rahulaswani.indianstartups;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by rahul on 22/04/15.
+ */
+public class DataManager extends Application {
+
+    public static final String TAG = DataManager.class.getSimpleName();
+
+    private static DataManager mDataManager;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mDataManager = this;
+    }
+
+    public static synchronized DataManager getInstance() {
+        return mDataManager;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue, new LruBitmapCache());
+        }
+        return this.mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
